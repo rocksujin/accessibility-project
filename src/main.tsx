f3a5e846e2954a62944164b19cab2fd754209969
kaya.ryu <kaya.ryu@asurion.com>
2026-06-17 14:17:37 +0900

    remove unnecessary files, use hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './theme.css'
-import './index.scss'
-import './App.scss'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import "./theme.css";
+import "./index.scss";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
-)
+);
